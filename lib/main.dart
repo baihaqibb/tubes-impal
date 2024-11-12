29772622f0a1528d1f9849b731a02c717b26ac3f
Putu Arjuna Nurgraha Eka Wana <67663965+WitchMomorency@users.noreply.github.com>
2024-11-12 22:23:22 +0700

    Update main.dart
    Tambahan dark mode dan light mode digunakan dengan system

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,20 @@ import 'package:flutter/material.dart';
 import 'home.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: LoginPage(),
-  ));
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(), // Tema terang//
+      darkTheme: ThemeData.dark(), // Tema gelap
+      themeMode: ThemeMode.system, // Mengatur tema sesuai dengan pengaturan sistem
+      home: const LoginPage(),
+    );
+  }
 }
