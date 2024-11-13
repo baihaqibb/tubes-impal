69a12fab9c0486df6cde115b780c504caf0a9ca9
Putu Arjuna Nurgraha Eka Wana <67663965+WitchMomorency@users.noreply.github.com>
2024-11-13 10:05:16 +0700

    Register done ver 0

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:simple_cal/notifications/notification.dart';
-//import 'package:simple_cal/pages/home_page.dart';
+import 'package:simple_cal/home.dart';
 import 'package:simple_cal/themes.dart';
 import 'package:timezone/data/latest.dart' as tz;
 
@@ -14,14 +14,13 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       darkTheme: Themes.dark,
       debugShowCheckedModeBanner: false,
       title: 'SimpleCalendar :)',
-      //home: const HomePage(),
+      home: const HomePage(), //Change home to the desired page
     );
   }
 }
