f2b39218ea54b0adc6459ec1d6e7f77e9d50fe9a
baihaqibb <138758831+baihaqibb@users.noreply.github.com>
2024-11-16 06:52:25 +0700

    Update Pages
    asdf

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:simple_cal/notifications/notification.dart';
-import 'package:simple_cal/registerPage.dart';
+import 'package:simple_cal/pages/register_page.dart';
 import 'package:simple_cal/pages/edit_page.dart';
 import 'package:simple_cal/pages/input_page.dart';
 import 'package:simple_cal/pages/home_page.dart';
 import 'package:simple_cal/themes.dart';
 import 'package:timezone/data/latest.dart' as tz;
-import 'home.dart';
+import 'pages/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
